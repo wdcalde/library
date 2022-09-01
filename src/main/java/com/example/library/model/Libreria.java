b60e086215ca7597fd9d92a8e4395f406b5b09d4
wdcalde <wdcalde@gmail.com>
2022-09-01 12:00:51 -0500

    implement data

---
+++ src/main/java/com/example/library/model/Libreria.java
@@ -0,0 +1,12 @@
+package com.example.library.model;
+
+import lombok.Data;
+
+public @Data class Libreria {
+
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String horario;
+
+}
